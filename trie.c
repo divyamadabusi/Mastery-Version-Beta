f25b6f040a213e565c4de02c8fc221bff7685123
Divya Madabusi <divyam@cs.utexas.edu>
2026-03-04 23:52:07 -0600

    done

--- trie.c
+++ trie.c
@@ -1,4 +1,5 @@
 #include "trie.h"
+
 #include <stdio.h>
 
 static void recurse_node(trie_node_t* root, char* word, size_t pos, bool* found_first);
@@ -31,14 +32,17 @@ void autocomplete(trie_node_t* root, char* prefix) {
         return;
     }
     bool found_first = false;
-    strncpy(word, prefix, len); // type man strncpy if you havent seen strncpy before
+    strncpy(word, prefix, len);  // type man strncpy if you havent seen strncpy before
     *(word + len) = '\0';
-    recurse_node(letter, word, (size_t) len, &found_first);
+    recurse_node(letter, word, (size_t)len, &found_first);
     printf("\n");
     free(word);
 }
 
 static void recurse_node(trie_node_t* root, char* word, size_t pos, bool* found_first) {
+    if (!root) {
+        return;
+    }
     if (root->isWord) {
         if (!*found_first) {
             printf("%s", word);
@@ -48,11 +52,14 @@ static void recurse_node(trie_node_t* root, char* word, size_t pos, bool* found_
         *found_first = true;
     }
     for (int i = 0; i < ASCII_CHARS - FIRST_VALID_CHAR; i++) {
-        *(word+pos) = i + FIRST_VALID_CHAR;
-        *(word+pos+1) = '\0';
+        *(word + pos) = i + FIRST_VALID_CHAR;
+        *(word + pos + 1) = '\0';
+        if (!root) {
+            return;
+        }
         recurse_node(root->children[i], word, pos + 1, found_first);
     }
-    *(word+pos) = '\0';
+    *(word + pos) = '\0';
 }
 
 /**
@@ -68,13 +75,13 @@ bool insert(trie_node_t* root, char* word) {
     int len = 0;
     while (*word && len < MAX_WORD_LENGTH) {
         if (!root->children[*word - FIRST_VALID_CHAR]) {
-            root->children[*word - FIRST_VALID_CHAR] = (trie_node_t*) calloc(1, sizeof(trie_node_t));
+            root->children[*word - FIRST_VALID_CHAR] = (trie_node_t*)calloc(1, sizeof(trie_node_t));
         }
         root = root->children[*word - FIRST_VALID_CHAR];
         word++;
         len++;
     }
-    bool res =  !root->isWord;
+    bool res = !root->isWord;
     root->isWord = true;
     return res;
 }
